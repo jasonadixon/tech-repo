49478e8be7dfd1252501f2ccdedb2775f3d076ac
Jason Dixon <jasonadixon@gmail.com>
2018-02-02 23:19:02 -0500

    Initial version of the deployment

---
+++ TechnologyRepository/src/main/java/net/dixonlabs/technologyrepository/TechnologyRepositoryApplication.java
@@ -0,0 +1,12 @@
+package net.dixonlabs.technologyrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyRepositoryApplication.class, args);
+	}
+}
---
+++ TechnologyRepository/src/test/java/net/dixonlabs/technologyrepository/TechnologyRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package net.dixonlabs.technologyrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechnologyRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
